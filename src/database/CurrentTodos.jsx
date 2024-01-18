fb9fdb8c0e04c578f34c4d863e0f54289423fb75
chaoting-sun <chaoting0108@gmail.com>
2024-01-18 18:27:24 +0800

    add database

---
+++ src/database/CurrentTodos.jsx
@@ -0,0 +1,14 @@
+const CurrentTodos = [
+  {
+    id: 0,
+    detail: "Clean my room before Chinese new year.",
+    isCompleted: false,
+  },
+  {
+    id: 1,
+    detail: "Learn to build a todo app in three days.",
+    isCompleted: false,
+  },
+];
+
+export default CurrentTodos;
