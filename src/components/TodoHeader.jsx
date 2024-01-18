78a722b6f6bcbb8d645165e58f66f9db4b5d3d2f
chaoting-sun <chaoting0108@gmail.com>
2024-01-18 16:49:25 +0800

    add color change of mode icon on hover

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
 import { DarkModeContext } from "../hooks/DarkModeContext";
 import styled from "styled-components";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 const Header = styled.div`
   width: 100%;
+  height: 50px;
   position: relative;
   display: flex;
   justify-content: space-between;
   margin-top: 80px;
   margin-bottom: 0px;
-`
+`;
 
 const Title = styled.div`
   height: 100%;
@@ -18,30 +21,41 @@ const Title = styled.div`
   font-family: "Josefin Sans", sans-serif;
   display: inline-block;
   user-select: none;
-`
+`;
 
 const Icon = styled.div`
   height: 100%;
   display: inline-block;
-`
+`;
 
-const IconImg = styled.img`
-  height: 40px;
-  user-select: none;
-  cursor: pointer;
-`
+const IconStyles = {
+  height: "100%",
+  fontSize: "2.8em",
+  color: "#ffffff",
+  userSelect: "none",
+  cursor: "pointer",
+};
 
 const TodoHeader = () => {
-  const {darkMode, toggleDarkMode} = useContext(DarkModeContext);
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
 
   return (
     <Header>
       <Title>TODO</Title>
       <Icon>
-        <IconImg
-          src={darkMode ? "/src/images/icon-sun.svg" : "/src/images/icon-moon.svg"}
-          onClick={toggleDarkMode}        
-        />
+        {darkMode ? (
+          <WbSunnyIcon
+            onClick={toggleDarkMode}
+            style={IconStyles}
+            sx={{ "&:hover": { color: "#ffaeae !important" } }}
+          />
+        ) : (
+          <DarkModeIcon
+            onClick={toggleDarkMode}
+            style={IconStyles}
+            sx={{ "&:hover": { color: "#fffaae !important" } }}
+          />
+        )}
       </Icon>
     </Header>
   );
